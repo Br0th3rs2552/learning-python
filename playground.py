6604255b1ccfddc24b271c4c0f0dcd6fe09f1977
Br0th3rs2552 <92bradshaw92@gmail.com>
2021-10-30 13:31:33 -0700

    getting there

---
+++ playground.py
@@ -0,0 +1,69 @@
+# print("yooo")
+
+# print(0)
+
+# lol = 1
+
+# print(lol)
+
+chud = 1 + 1
+
+rexy = 10 - 1
+
+asdf = 10 * 10
+
+laugh = 100/20
+
+gamer = 50 % 21
+
+fire = 5 // 2
+
+goku = 2 ** 10
+
+print(chud)
+print(rexy)
+print(asdf)
+print(laugh)
+print(gamer)
+print(fire)
+print(goku)
+
+debs = "cool "
+
+happy = " alot"
+
+runner = debs + happy
+
+print(runner)
+
+variable = debs * 100
+
+print(variable)
+
+debssubstring = debs[3]
+
+print(debssubstring)
+
+chucky = debs[2:4] * 10 + debs
+
+print(chucky)
+
+superman = f"chucky is a good villain: {chucky} {debssubstring}"
+
+print(superman)
+
+def foobar():
+    print("goo")
+
+foobar()
+
+def alex(example):
+    print(example)
+
+alex(superman)
+
+def multiple(flash, gordon):
+    gun = flash + gordon
+    print(gun)
+
+multiple(asdf, rexy)
